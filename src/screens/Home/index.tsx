656264672dfd224e46c140732427bbb8c1a21f63
Luciano Amâncio <madruga665@hotmail.com>
2021-10-22 14:52:40 -0300

    Hellow World react native 🚀️

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import { styles } from "./style";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text> Hellow World React Native! </Text>
+    </View>
+  );
+}
