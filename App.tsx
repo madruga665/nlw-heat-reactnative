07aacf9b0be281a9aee52e2c43d2f50b781c1d7e
Luciano Amâncio <madruga665@hotmail.com>
2021-10-22 15:14:49 -0300

    add expo-app-loading

--- App.tsx
+++ App.tsx
@@ -1,8 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Home } from './src/screens/Home';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <Home />
   );
