2c937ac5d8e15798a5eacd9f1a324faf9970a9ab
Luciano Amâncio <madruga665@hotmail.com>
2021-10-23 00:20:38 -0300

    add SendMessageForm

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,13 +4,14 @@ import { styles } from "./style";
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
 import { SignBox } from "../../components/SignBox";
+import { SendMessageForm } from "../../components/SendMessageForm";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Header />
       <MessageList />
-      <SignBox />
+      <SendMessageForm />
     </View>
   );
 }
