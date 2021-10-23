43fb01bc721e6b633c0af7fcb50795db829dca98
Luciano Amâncio <madruga665@hotmail.com>
2021-10-22 23:54:51 -0300

    Button finalizado

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,12 +3,14 @@ import { View } from "react-native";
 import { styles } from "./style";
 import { Header } from "../../components/Header";
 import { MessageList } from "../../components/MessageList";
+import { SignBox } from "../../components/SignBox";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Header />
       <MessageList />
+      <SignBox />
     </View>
   );
 }
